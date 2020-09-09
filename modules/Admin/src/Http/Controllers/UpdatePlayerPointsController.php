922cc3555102c466fb92ac563b336c7c9e160253
krsdata <kroy.iips@gmail.com>
2020-09-09 23:58:41 +0530

    admin

--- modules/Admin/src/Http/Controllers/UpdatePlayerPointsController.php
+++ modules/Admin/src/Http/Controllers/UpdatePlayerPointsController.php
@@ -92,7 +92,7 @@ class UpdatePlayerPointsController extends Controller {
             $updatePlayerPoints->transform(function($item,$key){
                 $match =  Match::where('match_id',$item->match_id)->first();
 
-                $item->match_title = $match->title??null;
+                $item->match_title = $match->short_title??null;
                 return $item; 
             });
         } else {
@@ -100,7 +100,7 @@ class UpdatePlayerPointsController extends Controller {
             $updatePlayerPoints->transform(function($item,$key){
                 $match =  Match::where('match_id',$item->match_id)->first();
 
-                $item->match_title = $match->title??null;
+                $item->match_title = $match->short_title??null;
                 return $item; 
             });
         }
