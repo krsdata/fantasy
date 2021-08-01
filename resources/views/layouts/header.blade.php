39aab4f1b1371762f67d2e221ecf1ee76f60651e
krsdata <kroy.iips@gmail.com>
2021-08-02 03:48:10 +0530

    new website

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>{{env('company_name')}} | Ninja11 Fantasy App</title>
+    <title>{{env('company_name')}} | Best Cricket Fantasy App</title>
     <meta charset="utf-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="content-type" content="text/html;charset=UTF-8">
