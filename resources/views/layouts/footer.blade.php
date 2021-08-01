42886c3ca79f9ff0e17b50d5ba6568f5e418bd10
krsdata <kroy.iips@gmail.com>
2021-08-02 03:36:02 +0530

    new website

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -127,7 +127,8 @@ fan base by keeping a watch over shared posts in the feed.</p>
 <script type="text/javascript" src="{{ URL::asset('webmedia/js/rev-slider/js/extensions/revolution.extension.migration.min.js')}}"></script> 
 <script type="text/javascript" src="{{ URL::asset('webmedia/js/rev-slider/js/extensions/revolution.extension.navigation.min.js')}}"></script> 
 <script type="text/javascript" src="{{ URL::asset('webmedia/js/rev-slider/js/extensions/revolution.extension.parallax.min.js')}}"></script> 
-<script type="text/javascript" src="{{ URL::asset('webmedia/js/rev-slider/js/extensions/revolution.extension.slideanims.min.js')}}"></script>  
+<script type="text/javascript" src="{{ URL::asset('webmedia/js/rev-slider/js/extensions/revolution.extension.slideanims.min.js')}}"></script> 
+<script type="text/javascript" src="{{ URL::asset('webmedia/js/rev-slider/js/extensions/revolution.extension.video.min.js')}}"></script>
 
 <script type="text/javascript">
   $(function(){
